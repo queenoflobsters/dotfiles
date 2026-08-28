d259cc5142f14291a1339475d7346c9188d7aead
gabribribribri <fuelanemo149@gmail.com>
2026-08-28 23:03:47 +0200

    [hypr] MORE

--- .config/hypr/keybinds.lua
+++ .config/hypr/keybinds.lua
@@ -1,15 +1,25 @@
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-
-
+function get_workspace(i)
+    local ws = hl.get_active_workspace()
+    if ws.monitor.id == 0 then
+      return i
+    else
+      if i <= 5 then
+        return i+5
+      else
+        return i-5
+      end
+    end
+  end
 -- Start applications
 hl.bind(MainMod .. " + Return", hl.dsp.exec_cmd(Terminal))
 hl.bind(MainMod .. " + Q", hl.dsp.exec_cmd(Browser))
 hl.bind(MainMod .. " + E", hl.dsp.exec_cmd(FileManager))
 
 -- Toggle panels
-hl.bind(MainMod .. " + Z", hl.dsp.exec_cmd( Ipc .. "panel-toggle wallpaper"))
-hl.bind(MainMod .. " + V", hl.dsp.exec_cmd( Ipc .. "panel-toggle clipboard"))
-hl.bind(MainMod .. " + X", hl.dsp.exec_cmd( Ipc .. "panel-toggle control-center"))
+hl.bind(MainMod .. " + Z", hl.dsp.exec_cmd(Ipc .. "panel-toggle wallpaper"))
+hl.bind(MainMod .. " + V", hl.dsp.exec_cmd(Ipc .. "panel-toggle clipboard"))
+hl.bind(MainMod .. " + X", hl.dsp.exec_cmd(Ipc .. "panel-toggle control-center"))
 hl.bind(MainMod .. " + Space", hl.dsp.exec_cmd(Ipc .. "panel-toggle launcher"))
 
 -- Kill the process owning the window with a SIGKILL
@@ -24,32 +34,47 @@ hl.bind(MainMod .. " + A", ToggleGaps)
 -- hl.bind(MainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(MainMod .. " + SHIFT + F", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(MainMod .. " + P", hl.dsp.window.pseudo())
-hl.bind(MainMod .. " + S", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(MainMod .. " + S", hl.dsp.layout("togglesplit")) -- dwindle only
 
 -- Move focus with mainMod + arrow keys
-hl.bind(MainMod .. " + H",  hl.dsp.focus({ direction = "left" }))
-hl.bind(MainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
-hl.bind(MainMod .. " + K",    hl.dsp.focus({ direction = "up" }))
+hl.bind(MainMod .. " + H", hl.dsp.focus({ direction = "left" }))
+hl.bind(MainMod .. " + J", hl.dsp.focus({ direction = "down" }))
+hl.bind(MainMod .. " + K", hl.dsp.focus({ direction = "up" }))
 hl.bind(MainMod .. " + L", hl.dsp.focus({ direction = "right" }))
 
 -- Resize windows
 local resizeUnit = 50
-hl.bind(MainMod .. " + SHIFT + H", hl.dsp.window.resize({ x = -resizeUnit, y = 0, relative=true }), { repeating = true })
-hl.bind(MainMod .. " + SHIFT + J", hl.dsp.window.resize({ x = 0, y = resizeUnit, relative=true }), { repeating = true })
-hl.bind(MainMod .. " + SHIFT + K", hl.dsp.window.resize({ x = 0, y = -resizeUnit, relative=true }), { repeating = true })
-hl.bind(MainMod .. " + SHIFT + L", hl.dsp.window.resize({ x = resizeUnit, y = 0, relative=true }), { repeating = true })
+hl.bind(MainMod .. " + CTRL + H", hl.dsp.window.resize({ x = -resizeUnit, y = 0, relative = true }),
+
+  { repeating = true })
+hl.bind(MainMod .. " + CTRL + J", hl.dsp.window.resize({ x = 0, y = resizeUnit, relative = true }), { repeating = true })
+hl.bind(MainMod .. " + CTRL + K", hl.dsp.window.resize({ x = 0, y = -resizeUnit, relative = true }),
+  { repeating = true })
+hl.bind(MainMod .. " + CTRL + L", hl.dsp.window.resize({ x = resizeUnit, y = 0, relative = true }), { repeating = true })
+
+-- Move Windows
+hl.bind(MainMod .. " + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
+hl.bind(MainMod .. " + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
+hl.bind(MainMod .. " + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
+hl.bind(MainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
 
 
+-- Workspaces manoeuoeveours 
 for i = 1, 10 do
-    local key = i % 10 -- 10 maps to key 0
-    -- Switch workspaces with mainMod + [0-9]
-    hl.bind(MainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-
-    -- Move window to a different workspace and switch to that workspace with mainMod + SHIFT + [0-9]
-    -- hl.bind(MainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
-    -- Move window to a different workspace but keep current workspace with mainMod + CTRL + [0-9]
-    hl.bind(MainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i, follow = false }))
+  local key = i%10
+  hl.bind(MainMod .. " + " .. key, function()
+    local w = get_workspace(i)
+    hl.dispatch(hl.dsp.focus({ workspace = w }))
+  end)
+  hl.bind(MainMod .. " + SHIFT + " .. key, function()
+    local w = get_workspace(i)
+    hl.dispatch(hl.dsp.window.move({ workspace = w, follow = true }))
+    end
+  )
 end
+
+-- Special workspaces
+hl.bind(MainMod .. " + M", hl.dsp.workspace.toggle_special("Music"))
 
 -- Maximize - Window takes up the entire working space, keeping the margins.
 hl.bind(MainMod .. " + G", hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }))
@@ -62,24 +87,27 @@ hl.bind("PRINT", hl.dsp.exec_cmd(Ipc .. "screenshot-fullscreen pick"))
 hl.bind(MainMod .. " +  ", hl.dsp.exec_cmd(Ipc .. "screenshot-region"))
 
 -- Example special workspace (scratchpad)
-hl.bind(MainMod .. "",         hl.dsp.workspace.toggle_special("magic"))
+hl.bind(MainMod .. "", hl.dsp.workspace.toggle_special("magic"))
 hl.bind(MainMod .. "", hl.dsp.window.move({ workspace = "special:magic" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
-hl.bind(MainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(MainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind(MainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Laptop multimedia keys for volume and LCD brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+  { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+  { locked = true, repeating = true })
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+  { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
 -- Requires playerctl
-hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
-hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
-
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
--- .config/hypr/workspaces.lua
+++ .config/hypr/workspaces.lua
@@ -1,23 +1,16 @@
 -- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
--- "Smart gaps" / "No gaps when only"
--- uncomment all if you wish to use that.
--- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
--- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
--- hl.window_rule({
---     name  = "no-gaps-f1",
---     match = { float = false, workspace = "f[1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
 
-hl.config({
-    binds = {
-        workspace_back_and_forth = true
-    },
-})
+hl.workspace_rule({ workspace = "1", monitor = "DP-1", default = true})
+hl.workspace_rule({ workspace = "2", monitor = "DP-1"})
+hl.workspace_rule({ workspace = "3", monitor = "DP-1"})
+hl.workspace_rule({ workspace = "4", monitor = "DP-1"})
+hl.workspace_rule({ workspace = "5", monitor = "DP-1"})
+hl.workspace_rule({ workspace = "6", monitor = "DP-2", default = true})
+hl.workspace_rule({ workspace = "7", monitor = "DP-2"})
+hl.workspace_rule({ workspace = "8", monitor = "DP-2"})
+hl.workspace_rule({ workspace = "9", monitor = "DP-2"})
+hl.workspace_rule({ workspace = "10", monitor = "DP-2"})
+
+
+-- Special
+hl.workspace_rule({workspace = "special:Music", gaps_in = 10, gaps_out = 40, on_created_empty = "flatpak run dev.aunetx.deezer"})
